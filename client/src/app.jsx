07714b3189ff115a518d5501063a45f7edfede0e
Grace <gracelo0717@gmail.com>
2024-07-02 17:57:45 -0400

    add new route for info pg

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/login.jsx';
 import Home from './components/home.jsx';
+import Info from './components/info.jsx';
 
 export default function App() {
   return (
@@ -12,11 +13,14 @@ export default function App() {
           <Link to='/'>Home</Link>
           <br></br>
           <Link to='/login'>Login</Link>
+          <br></br>
+          <Link to='/info'>Getting Started</Link>
         </nav>
 
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/info' element={<Info />} />
         </Routes>
       </div>
     </Router>
