9ded40faf3bad8b1e183ad732310a4cd8e94bed1
Grace <gracelo0717@gmail.com>
2024-07-02 16:32:29 -0400

    update home and login page using router

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/login.jsx';
+import Home from './components/home.jsx';
 
 export default function App() {
   return (
-    <div>
-      <h1 className='header'>DeClustor</h1>
-    </div>
+    <Router>
+      <div>
+        <h1 className='header'>DeClustor</h1>
+        <nav>
+          <Link to='/'>Home</Link>
+          <br></br>
+          <Link to='/login'>Login</Link>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Login = () => {
-  return <h1>Hello World!</h1>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className='login-container'>
+      <form onSubmit={handleSubmit} className='login-form'>
+        <div className='form-group'>
+          <label htmlFor='username'>Username:</label>
+          <input
+            type='text'
+            id='username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='password'
+            id='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
