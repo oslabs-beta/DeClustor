bd11eb4373d449b922d895e3480e6dcbcfb786ed
Ploynapa Kham <ploynp.k@outlook.com>
2024-07-15 17:02:20 -0700

    notification setting done

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import globalReducer from './globalSlice.js';
 import userReducer from './userSlice.js'; 
-
+import notificationReducer from './notificationSlice.js'
 // redux store
 const store = configureStore({
     // call reducer
     reducer: {
       global: globalReducer,
       user: userReducer,
+      notification: notificationReducer,
     },
 
 });
