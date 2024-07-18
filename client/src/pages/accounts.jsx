77fb971e03ccd986ca233779860642e1a9c524d3
Grace <gracelo0717@gmail.com>
2024-07-18 01:38:22 -0400

    updates

---
+++ client/src/pages/accounts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Accounts = () => {
+  return (
+    <div>
+      <h2>Info Component</h2>
+      <p>This is the info page.</p>
+    </div>
+  );
+};
+
+export default Accounts;
